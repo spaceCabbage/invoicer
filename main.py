ea33d79a47e2be65c1df89f8456240316715fac3
yehudafreedman <hudafreedman@gmail.com>
2022-09-21 17:06:35 +0300

    pandas

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
-from openpyxl import Workbook, load_workbook
-from openpyxl.utils import get_column_letter
+import pandas as pd
+import numpy as np
 
 # choose file 
 # report = input(enter path to this weeks report:)
-# report = 'sample_report.xlsx'
+report = 'sample_report.csv'
 
-wb = load_workbook('sample_report.xlsx')
-ws = wb.active
+report_data = pd.read_csv(report)
+lookup = pd.read_csv('sku_replacement.csv')
+
+# merge (vlookup) from lookup.csv
+# new sku and multiplier
+
+# check for errors (😬)
+
+# add new replacement sku to sku_replacement.csv for next time
+
+# multiply qty by multiplier
+
+# set all negative adjustments as refund type
+
+# pivot report_data on new sku, total new qty, total price
+
+# any skus of type order and of marketplace Ebay must be deducted from qty (not price total)
+
+# export properly formatted invoice as per qb requirements
+
+# export credit memo of all returns and fees to be entered manually (💩)
 
 
-for row in range(1, 102):
-    print(ws['D' + str(row)].value)
