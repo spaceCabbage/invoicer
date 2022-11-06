abc4e22001bacf752685302ac4d1bccd9b02bee1
sapceCabbage <hudafreedman@gmail.com>
2022-11-06 19:19:41 +0200

    test

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ import warnings
 
 warnings.filterwarnings("ignore", category=FutureWarning)
 
-
 # ? choose file
 
 # GUI file picker
@@ -24,7 +23,6 @@ lookup = pd.read_csv("sku_replacement.csv")
 #! date picker
 date_start = "4/4/2020"
 date_end = "4/10/2020"
-
 
 # ? get all skus that are not found in lookup df
 print("\nLoading Lookup Table....")
